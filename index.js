0a3fd9b5c42238661880080c99c59bb5276f88b9
Codi Kraus <codi.kraus@gmail.com>
2021-09-21 15:42:40 -0500

    commit tree b4f4fc76d0a419da01d2d033992e2d8e64dde872

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+function superbowlWin(arrayOfObjects) {
+    if (arrayOfObjects.find(object => object.result === "W") === undefined) {
+      return undefined;
+    } else {
+      return arrayOfObjects.find(object => object.result === "W").year;
+    }
+  }
